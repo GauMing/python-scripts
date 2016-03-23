694a2881d721faae76ad9975a346423e6544846b
Kohaku <gmkohaku@gmail.com>
2016-03-23 20:22:03 +0800

    add zhihu.py

--- signin_so.py
+++ signin_so.py
@@ -10,35 +10,37 @@
 import requests
 import time
 
+
 class stackoverflow(object):
-	s = requests.Session()
+    s = requests.Session()
 
-	def signin(self):
-		url = 'https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f%2fstackoverflow.com%2f'
-		payload = {
-			'fkey': '',
-			'ssrc': 'head',
-			'email': '@',
-			'password': '',
-			'oauth-version': '',
-			'oauth-server': '',
-			'openid-username': '',
-			'openid-identifier': ''
-		}
+    def signin(self):
+        url = 'https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f%2fstackoverflow.com%2f'
+        payload = {
+            'fkey': '',
+            'ssrc': 'head',
+            'email': '@',
+            'password': '',
+            'oauth-version': '',
+            'oauth-server': '',
+            'openid-username': '',
+            'openid-identifier': ''
+        }
 
-		headers = {
-			'referer': 'https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f%2fstackoverflow.com%2f'
-		}
+        headers = {
+            'referer': 'https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f%2fstackoverflow.com%2f'
+        }
 
-		r = self.s.post(url, data=payload, headers=headers)
-		if 'logout' in r.content and 'kohaku' in r.content:
-			ret = "Success"
-		else:
-			ret = "Fail"
+        r = self.s.post(url, data=payload, headers=headers)
+        if 'logout' in r.content and 'kohaku' in r.content:
+            ret = "Success"
+        else:
+            ret = "Fail"
 
-		filepath = 'soSigninLog.log'
-		with open(filepath, 'a') as f:
-			f.write(time.ctime() + ' '*10 + ret + '\n')
+        filepath = 'soSigninLog.log'
+        with open(filepath, 'a') as f:
+            f.write(time.ctime() + ' ' * 10 + ret + '\n')
+
 
 so = stackoverflow()
 so.signin()
