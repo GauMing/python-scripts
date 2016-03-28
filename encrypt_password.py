ae340ac223c0ec67437171bbf781daa7f4fe5383
Kohaku <gmkohaku@gmail.com>
2016-03-28 18:44:25 +0800

    update encrypt_password.py

--- encrypt_password.py
+++ encrypt_password.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import hashlib
-import hmac
 
 import gen_random_key
 
@@ -15,14 +14,12 @@ encrypt and validate password
 def encrypt_password(raw_password, salt=None):
     if not salt:
         salt = gen_random_key.random_key(1, 8).pop()
-    # result = hmac.HMAC(raw_password, salt, hashlib.sha256).hexdigest()
-    result = hashlib.md5(salt + raw_password).hexdigest()
+    result = hashlib.sha256(salt + raw_password).hexdigest()
     return salt + result
-
 
 
 def validate_password(encrypted, input_password):
     return encrypted == encrypt_password(input_password, encrypted[:8])
 
 
-print validate_password(encrypt_password('hello'), 'hello1')
+print validate_password(encrypt_password('hello'), 'hello')
