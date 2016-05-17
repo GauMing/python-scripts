64f651b75b2c6f92f438a77a54e8918e5032533d
Kohaku <gmkohaku@gmail.com>
2016-05-17 10:47:41 +0800

    resize the heart

--- concat_pics.py
+++ concat_pics.py
@@ -25,7 +25,7 @@ def resizePics(dir, resizeX, resizeY):
 
 
 def renameFiles(dir):
-    index = 100
+    index = 1
     pics = os.listdir(dir)
     for pic in pics:
         name_after = dir + str(index) + '.jpg'
@@ -34,31 +34,40 @@ def renameFiles(dir):
         index += 1
 
 
-def concatPics(dir):
-    toImageWidth = 2000
-    toImageHeight = 1000
-    index = 100
+def concatPics(dir, imgCount):
+    toImageWidth = 2500
+    toImageHeight = 1600
+    index = 1
     cube_size = 100
     toImage = Image.new('RGBA', (toImageWidth, toImageHeight))
-    for y in xrange(10):
-        for x in xrange(20):
-            pic = dir + str(random.randint(100, 199)) + '.jpg'
-            fromImage = Image.open(pic)
-            toImage.paste(fromImage, (x * cube_size, y * cube_size))
-            index += 1
-    theHeart = [(8, 2), (9, 2), (11, 2), (12, 2), (7, 3), (8, 3), (9, 3), (10, 3), (11, 3), (12, 3), (13, 3), (7, 4),
-                (8, 4), (9, 4), (10, 4), (11, 4), (12, 4), (13, 4), (8, 5), (9, 5), (10, 5), (11, 5), (12, 5), (9, 6),
-                (10, 6), (11, 6), (10, 6), (10, 7)]
-    for point in theHeart:
-        blankImage = Image.open(dir + 'blank.jpg')
-        toImage.paste(blankImage, (point[0] * cube_size, point[1] * cube_size))
+    blankImage = Image.open(dir + 'blank.jpg')
+    theHeart = [(3, 1), (4, 1), (11, 1), (12, 1), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (9, 2), (10, 2), (11, 2),
+                (12, 2), (13, 2), (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3), (10, 3),
+                (11, 3), (12, 3), (13, 3), (14, 3), (1, 4), (2, 4), (3, 4), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4),
+                (9, 4), (10, 4), (11, 4), (12, 4), (13, 4), (14, 4), (1, 5), (2, 5), (3, 5), (4, 5), (5, 5), (6, 5),
+                (7, 5), (8, 5), (9, 5), (10, 5), (11, 5), (12, 5), (13, 5), (14, 5), (2, 6), (3, 6), (4, 6), (5, 6),
+                (6, 6), (7, 6), (8, 6), (9, 6), (10, 6), (11, 6), (12, 6), (13, 6), (3, 7), (4, 7), (5, 7), (6, 7),
+                (7, 7), (8, 7), (9, 7), (10, 7), (11, 7), (12, 7), (4, 8), (5, 8), (6, 8), (7, 8), (8, 8), (9, 8),
+                (10, 8), (11, 8), (5, 9), (6, 9), (7, 9), (8, 9), (9, 9), (10, 9), (6, 10), (7, 10), (8, 10), (9, 10),
+                (7, 11), (8, 11)]
+    offsetX = 4
+    offsetY = 1
+    for y in xrange(16):
+        for x in xrange(25):
+            if (x - offsetX, y - offsetY) in theHeart:
+                toImage.paste(blankImage, (x * cube_size, y * cube_size))
+            else:
+                pic = dir + str(random.randint(1, imgCount)) + '.jpg'
+                fromImage = Image.open(pic)
+                toImage.paste(fromImage, (x * cube_size, y * cube_size))
+                index += 1
     toImage.save('./result.jpg')
 
 
 dir = './img/'
-renameFiles(dir)
-resizePics(dir, 100, 100)
-concatPics(dir)
+# renameFiles(dir)
+# resizePics(dir, 100, 100)
+concatPics(dir, 100)
 
 # init blank
 # resizePics('./blank/', 100, 100)
